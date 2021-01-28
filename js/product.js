f4d6a51de8d004dadd0f3109038ae26ae965e18b
Torbjørn Haukås <69842749+Hawkas@users.noreply.github.com>
2021-01-28 15:18:01 +0100

    done

--- js/product.js
+++ js/product.js
@@ -1,5 +1,25 @@
-const product = {
+const product = [
+  {
     name: "Fancy Product",
     price: 99,
     onSale: true,
-};
+  },
+];
+const out = document.querySelector("div.container");
+for (let item of product) {
+  let innerContainer = document.createElement("div");
+  innerContainer.classList.add("product");
+  let prodName = document.createElement("h2");
+  let priceTag = document.createElement("p");
+  let price = "n/a";
+  prodName.innerHTML = item.name;
+  if (item.price) {
+    price = item.price;
+  }
+  priceTag.innerHTML = "Price: " + price;
+  if (item.onSale) {
+    prodName.style.textDecoration = "underline";
+  }
+  innerContainer.innerHTML = prodName.outerHTML + priceTag.outerHTML;
+  out.innerHTML += innerContainer.outerHTML;
+}
--- js/products.js
+++ js/products.js
@@ -1,16 +1,36 @@
 const products = [
-    {
-        name: "Fancy Product",
-        price: 40,
-        soldOut: true,
-    },
-    {
-        name: "Another Product",
-        soldOut: false,
-    },
-    {
-        name: "Cheap Product",
-        price: 10,
-        soldOut: true,
-    },
+  {
+    name: "Fancy Product",
+    price: 40,
+    soldOut: true,
+  },
+  {
+    name: "Another Product",
+    soldOut: false,
+  },
+  {
+    name: "Cheap Product",
+    price: 10,
+    soldOut: true,
+  },
 ];
+const out = document.querySelector("div.container");
+for (let product of products) {
+  let innerContainer = document.createElement("div");
+  innerContainer.classList.add("product");
+  let prodName = document.createElement("h2");
+  let priceTag = document.createElement("p");
+  let price = "n/a";
+  prodName.innerHTML = product.name;
+  if (product.price) {
+    price = product.price;
+  }
+  priceTag.innerHTML = "Price: " + price;
+  if (product.soldOut) {
+    prodName.style.color = "red";
+  } else {
+    prodName.style.color = "green";
+  }
+  innerContainer.innerHTML = prodName.outerHTML + priceTag.outerHTML;
+  out.innerHTML += innerContainer.outerHTML;
+}
